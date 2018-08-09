6b5c3a08ae236da7d3c0c6997cdb2fc90e5b2e47
Stephen Giordano <steve1_nyc@yahoo.com>
2018-08-09 15:44:10 -0400

    basic react-native app that runs on android and ios

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends React.Component {
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <Text>SlateFinder</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
